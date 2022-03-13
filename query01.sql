d5bf64206f2c4aacd158d9da0bca9ef55e0da114
Jai Dhaliwal <91633223+realjaidhaliwal@users.noreply.github.com>
2022-03-13 10:54:41 -0700

    Add files via upload

---
+++ query01.sql
@@ -0,0 +1,8 @@
+-- Retrieve the name and Date of all Badges obtained by
+-- the user with ID 3 awarded after 2011,
+-- ordered by ascending date
+-- 1.1 marks: <5 operators
+-- 1.0 marks: <6 operators
+-- 0.8 marks: correct answer
+
+-- Replace this comment line with the actual query
